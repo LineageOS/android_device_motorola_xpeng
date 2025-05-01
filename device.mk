f4b96ffdb74bcdb028fd27331f5a2bb036bad8cc
Michael Bestas <mkbestas@lineageos.org>
2025-05-01 10:46:20 +0300

    xpeng: Override properties according to stock
    Change-Id: I46fff8c728cc61bc5a105c1202b91998b1a23640

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Properties
+PRODUCT_PACKAGES += \
+    hardware.sku.XT2175-2.prop
+
 # Sensors
 $(call soong_config_set_bool,moto_sensors,udfps,false)
 
