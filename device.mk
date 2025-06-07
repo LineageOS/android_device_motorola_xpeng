635964dfecc906baf9f69202f4740363db814ef4
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    xpeng: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- device.mk
+++ device.mk
@@ -101,5 +101,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-xpeng-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-xpeng.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.motorola
+
+$(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/xpeng/xpeng-vendor.mk)
