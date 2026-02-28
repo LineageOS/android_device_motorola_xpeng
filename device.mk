c731cd6f1e3a51ce15c23357fc9c682ced0842ae
Marc Bourgoin <themard85@gmail.com>
2026-02-28 12:35:48 -0700

    xpeng: Declare SoC is lahaina for powerhint
    Change-Id: I80aa9e6388397e1f73efb1a26e648cb3bdccb074

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# SoC
+TARGET_IS_LAHAINA := true
+
 # Inherit from motorola sm7325-common
 $(call inherit-product, device/motorola/sm7325-common/common.mk)
 
