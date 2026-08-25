c75c230ae4f3c46ca7e16cd8bb42c39376625bf6
Marc Bourgoin <themard85@gmail.com>
2026-08-25 02:37:13 -0600

    xpeng: Correct build fingerprint and description
    Change-Id: I47a0aa5ee619920bd8cce2d88cae29e2ff49a2ba

--- lineage_xpeng.mk
+++ lineage_xpeng.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := moto g200 5G
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="xpeng_g-user 12 S1RXS32.50-13-25 5fb68-c44485 release-keys" \
-    BuildFingerprint=motorola/xpeng_g/xpeng:12/S1RXS32.50-13-25/5fb68-c44485:user/release-keys \
+    BuildDesc="xpeng_g-user 12 S1RXS32.50-13-25 c44485-5fb68 release-keys" \
+    BuildFingerprint=motorola/xpeng_g/xpeng:12/S1RXS32.50-13-25/c44485-5fb68:user/release-keys \
     DeviceProduct=xpeng_retail
